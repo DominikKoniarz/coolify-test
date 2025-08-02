b19c53340e23f558b5159b1c1cf5b16bd1a77cdd
DominikKoniarz <dominik.koniarz.development@gmail.com>
2025-08-02 23:18:00 +0200

    Change styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
-  return (
-    <main className="grid place-items-center bg-slate-800">
-    xdxdxdxdxd coolify test
-    </main>
-  );
+	return (
+		<main className="grid h-full text-lg place-items-center bg-slate-800">
+			xdxdxdxdxd coolify test
+		</main>
+	);
 }
